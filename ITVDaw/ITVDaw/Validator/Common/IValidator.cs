211379a375a73e00ba2f241f77d577ff8ae17dee
Javier Callejo <javiercalo@hotmail.es>
2026-04-17 18:21:47 +0200

    Añadido Validador y terminado extensiones

---
+++ ITVDaw/ITVDaw/Validator/Common/IValidator.cs
@@ -0,0 +1,9 @@
+﻿using CSharpFunctionalExtensions;
+using ITVDaw.Errors.Common;
+
+namespace ITVDaw.Validator.Common;
+
+public interface IValidator<T>
+{
+    Result<T, DomainError> Validar(T Entidad);
+}
---
+++ ITVDaw/ITVDaw/Validator/Vehiculos/ValidadorVehiculos.cs
@@ -0,0 +1,34 @@
+﻿using System.Text.RegularExpressions;
+using CSharpFunctionalExtensions;
+using ITVDaw.Errors.Common;
+using ITVDaw.Errors.Vehiculos;
+using ITVDaw.Models;
+using ITVDaw.Validator.Common;
+
+namespace ITVDaw.Validator.Vehiculos;
+
+public class ValidadorVehiculos : IValidator<Vehiculo>
+{
+    public Result<Vehiculo, DomainError> Validar(Vehiculo vehiculo)
+    {
+        // var PatronDni = @"^\d{8}$";
+        var PatronMatricula = @"^\d{4}[BCDFGHJKLMNPRSTVWXYZ]{3}$";
+        var Errors = new List<string>();
+
+        if (vehiculo is not Vehiculo Vehiculo)
+        {
+            Errors.Add("La entidad vehiculo insertada no es valida");
+            return Result.Failure<Vehiculo, DomainError>(VehiculosErrors.Validation(Errors));
+        }
+
+        if (vehiculo.Cilindrada > 5000 || vehiculo.Cilindrada < 0)
+            Errors.Add("La Cilindrada debe estar entre 0 y 5000");
+
+        if (string.IsNullOrWhiteSpace(vehiculo.Marca)) Errors.Add("El campo marca no puede estar en blanco");
+
+        if (string.IsNullOrWhiteSpace(vehiculo.Matricula) || Regex.IsMatch(vehiculo.Matricula, PatronMatricula))
+            Errors.Add("El campo matricula no puede estar en blanco, o no ser Letra-Numero [LLLLNNN]");
+
+        return Result.Success<Vehiculo, DomainError>(vehiculo);
+    }
+}
